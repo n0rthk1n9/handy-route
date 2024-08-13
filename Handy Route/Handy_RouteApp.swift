952abafb6c832456525bda6826bee6b2e18bcc62
Jan Armbrust <jan.armbrust@googlemail.com>
2024-08-13 16:06:53 +0200

    Initial Commit

---
+++ Handy Route/Handy_RouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handy_RouteApp.swift
+//  Handy Route
+//
+//  Created by Jan Armbrust on 13.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Handy_RouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
